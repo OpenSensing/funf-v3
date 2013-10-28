040c723320c6de400b798b7fb21d6e3332796190
sapiezynski <sapiezynski@gmail.com>
2013-10-28 15:57:12 +0100

    Added handling url messages from notfiaction bar

--- datacollector/src/dk/dtu/imm/datacollector2013/GcmBroadcastReceiver.java
+++ datacollector/src/dk/dtu/imm/datacollector2013/GcmBroadcastReceiver.java
@@ -14,6 +14,8 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
@@ -45,10 +47,17 @@ public class GcmBroadcastReceiver extends BroadcastReceiver {
                     intent.getExtras().toString());
         } else {
         	Intent notifyIntent = new Intent(EVENT_MSG_RECEIVED);
-        	notifyIntent.putExtras(intent.getExtras());
+            Bundle extras = intent.getExtras();
+        	notifyIntent.putExtras(extras);
         	LocalBroadcastManager.getInstance(context).sendBroadcast(notifyIntent);
-            sendNotification(intent.getExtras().getString("title", "SensibleDTU"),
-                    intent.getExtras().getString("message"));
+            if (extras.getString("type", "").equals("url")) {
+                sendUrlNotification(extras.getString("title"),
+                        extras.getString("message"),
+                        extras.getString("url"));
+            } else {
+                sendNotification(intent.getExtras().getString("title", "SensibleDTU"),
+                        intent.getExtras().getString("message"));
+            }
         }
         setResultCode(Activity.RESULT_OK);
     }
@@ -68,6 +77,26 @@ public class GcmBroadcastReceiver extends BroadcastReceiver {
                         .setStyle(new NotificationCompat.BigTextStyle()
                                 .bigText(msg))
                         .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }    // Put the GCM message into a notification and post it.
+
+    private void sendUrlNotification(String title, String msg, String url) {
+        mNotificationManager = (NotificationManager)
+                ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0,
+                new Intent(Intent.ACTION_VIEW, Uri.parse(url)), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(ctx)
+                        .setSmallIcon(R.drawable.red_logo5)
+                        .setContentTitle(title)
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(msg))
+                        .setContentText(msg)
+                        .setAutoCancel(true);
 
         mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
