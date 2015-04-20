bb237451a900f4e71aa35e730acfba678f937b7f
radugatej <radu.gatej@gmail.com>
2015-04-20 22:16:52 +0200

    Changed file upload method in order to address crash on large file upload.

--- funf-open-sensing-framework/src/main/java/edu/mit/media/funf/storage/HttpArchive.java
+++ funf-open-sensing-framework/src/main/java/edu/mit/media/funf/storage/HttpArchive.java
@@ -26,11 +26,24 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import android.net.Uri;
 import android.util.Log;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import static edu.mit.media.funf.Utils.TAG;
 /**
  * Archives a file to the url specified using POST HTTP method.
@@ -122,98 +135,38 @@ public class HttpArchive implements RemoteArchive {
 		Log.d(TAG, "Valid url? " + isValidUrl);
 		return isValidUrl;
 	}
-	
-	/**
-	 * Copied (and slightly modified) from Friends and Family
-	 * @param file
-	 * @param uploadurl
-	 * @return
-	 */
-	public static boolean uploadFile(File file,String uploadurl) {
-		HttpURLConnection conn = null; 
-		DataOutputStream dos = null; 
-		DataInputStream inStream = null; 
 
-		String lineEnd = "\r\n"; 
-		String twoHyphens = "--"; 
-		String boundary =  "*****"; 
+    public static boolean uploadFile(File file, String uploadurl) {
+        HttpClient httpClient = new DefaultHttpClient() ;
 
+        HttpPost httpPost = new HttpPost(uploadurl);
+        MultipartEntity entity = new MultipartEntity();
+        entity.addPart("uploadedfile", new FileBody(file));
+        httpPost.setEntity(entity );
+        HttpResponse response = null;
 
-		int bytesRead, bytesAvailable, bufferSize; 
-		byte[] buffer; 
-		int maxBufferSize = 64*1024; //old value 1024*1024 
+        try {
+            response = httpClient.execute(httpPost);
+        } catch (ClientProtocolException e) {
+            Log.e("ClientProtocolException : "+e, e.getMessage());
+            return false;
+        } catch (IOException e) {
+            Log.e("IOException : "+e, e.getMessage());
+            return false;
 
-		boolean isSuccess = true;
-		//------------------ CLIENT REQUEST 
-		FileInputStream fileInputStream = null;
-		//Log.i("FNF","UploadService Runnable: 1"); 
-		try {
-			fileInputStream = new FileInputStream(file);
-			// open a URL connection to the Servlet 
-			URL url = new URL(uploadurl); 
-			// Open a HTTP connection to the URL 
-			conn = (HttpURLConnection) url.openConnection(); 
-			// Allow Inputs 
-			conn.setDoInput(true); 
-			// Allow Outputs 
-			conn.setDoOutput(true); 
-			// Don't use a cached copy. 
-			conn.setUseCaches(false); 
-			// set timeout
-			conn.setConnectTimeout(60000);
-			conn.setReadTimeout(60000);
-			// Use a post method. 
-			conn.setRequestMethod("POST"); 
-			conn.setRequestProperty("Connection", "Keep-Alive"); 
-			conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary); 
+        }
 
-			dos = new DataOutputStream( conn.getOutputStream() ); 
-			dos.writeBytes(twoHyphens + boundary + lineEnd); 
-			dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + file.getName() +"\"" + lineEnd); 
-			dos.writeBytes(lineEnd); 
+        if(response == null) {
+            return false;
+        }
 
-			//Log.i("FNF","UploadService Runnable:Headers are written"); 
-
-			// create a buffer of maximum size 
-			bytesAvailable = fileInputStream.available(); 
-			bufferSize = Math.min(bytesAvailable, maxBufferSize); 
-			buffer = new byte[bufferSize]; 
-
-			// read file and write it into form... 
-			bytesRead = fileInputStream.read(buffer, 0, bufferSize); 
-			while (bytesRead > 0) 
-			{ 
-				dos.write(buffer, 0, bufferSize); 
-				bytesAvailable = fileInputStream.available(); 
-				bufferSize = Math.min(bytesAvailable, maxBufferSize); 
-				bytesRead = fileInputStream.read(buffer, 0, bufferSize); 
-			} 
-
-			// send multipart form data necesssary after file data... 
-			dos.writeBytes(lineEnd); 
-			dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd); 
-
-			// close streams 
-			//Log.i("FNF","UploadService Runnable:File is written"); 
-			fileInputStream.close(); 
-			dos.flush(); 
-			dos.close(); 
-			//------------------ read the SERVER RESPONSE 
-			if (conn.getResponseCode() != 200) {
-				isSuccess = false;
-			}
-		}catch (FileNotFoundException e) {
-			Log.e(TAG, "file not found", e);
-			isSuccess = false;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			Log.e("FNF", "UploadService Runnable:Client Request error", e);
-			isSuccess = false;
-		}
-			
+        if(response.getStatusLine().getStatusCode() == 200) {
+            return true;
+        }
+        return false;
+    }
 
 
 
-		return isSuccess;
-	}
 }
+
